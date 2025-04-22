bcf495440d6ca3c3123d3ce7856975727079b3c8
pedroromao3 <pedroromao.pmr@gmail.com>
2025-04-22 18:31:01 +0100

    slave changes

--- spi_slave_2/include/SPISlave_T4.h
+++ spi_slave_2/include/SPISlave_T4.h
@@ -6,7 +6,6 @@
 #include <functional>
 #include <SPI.h>
 
-#if defined(__IMXRT1062__)
 #define SLAVE_CR spiAddr[4]
 #define SLAVE_FCR spiAddr[22]
 #define SLAVE_FSR spiAddr[23]
@@ -18,48 +17,11 @@
 #define SLAVE_SR spiAddr[5]
 #define SLAVE_TCR_REFRESH spiAddr[24] = (2UL << 27) | LPSPI_TCR_FRAMESZ(16 - 1)
 
-#elif defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__)
-#define SLAVE_MCR spiAddr[0]
-#define SLAVE_CTAR0 spiAddr[3]
-#define SLAVE_RSER spiAddr[12]
-#define SLAVE_TDR(x) spiAddr[13] = (x) // SPIx_PUSHR_SLAVE
-#define SLAVE_RDR spiAddr[14] // SPIx_POPR
-#define SLAVE_SR spiAddr[11]
 
-#elif defined(KINETISL)
-#define SLAVE_S spiAddr[0]
-#define SLAVE_DL spiAddr[6]
-#define SLAVE_DH spiAddr[7]
-#define SLAVE_C1 spiAddr[3]
-#define SLAVE_C2 spiAddr[2]
-#define SLAVE_BR spiAddr[1]
-#define SLAVE_RDR (((uint16_t)(SLAVE_DH << 8)) | SLAVE_DL)
-#define SLAVE_TDR(x) \
-    while ( !::digitalReadFast(10) && !(SLAVE_S & SPI_S_SPTEF) ); \
-    (SLAVE_DH = (uint8_t)((x) >> 8)); \
-    (SLAVE_DL = ((uint8_t)(x)));
-#endif
-
-
-
-#if defined(__IMXRT1062__)
 #define SPI_WAIT_STATE \
     while ( !(SLAVE_SR & (1UL << 9)) ) { /* FCF: Frame Complete Flag, set when PCS deasserts */ \
       if ( !(SLAVE_FSR & 0x1F0000) ) continue; /* wait for received data */ \
       if ( (SLAVE_SR & (1UL << 8)) ) { /* WCF set */
-
-#elif defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__)
-#define SPI_WAIT_STATE \
-      while ( !::digitalReadFast(10) ) { \
-        if ( SLAVE_SR & 0xF0 ) {
-
-#elif defined(KINETISL)
-#define SPI_WAIT_STATE \
-      while ( !::digitalReadFast(10) ) { \
-        if ( SLAVE_S & SPI_S_SPRF ) {
-#endif
-
-
 
 
 #define SPI_ENDWAIT_STATE \
@@ -67,33 +29,19 @@
     }
 
 
-
-
-#if defined(__IMXRT1062__)
 #define SPI_ISR_EXIT \
     SLAVE_SR = 0x3F00; \
     asm volatile ("dsb"); \
     return;
-#elif defined(__MK20DX256__) || defined(__MK64FX512__) || defined(__MK66FX1M0__)
-#define SPI_ISR_EXIT \
-    SLAVE_SR |= SPI_SR_RFDF; \
-    asm volatile ("dsb"); \
-    return;
-#elif defined(KINETISL)
-#define SPI_ISR_EXIT \
-    asm volatile ("dsb"); \
-    return;
-#endif
-
-
 
 
 #define SPI_MST_QUEUE_SLOTS 8
 #define SPI_MST_DATA_BUFFER_MAX 100
 
-struct AsyncMST {
-  uint16_t packetID = 0;
-  uint16_t slaveID = 0;
+struct AsyncMST
+{
+    uint16_t packetID = 0;
+    uint16_t slaveID = 0;
 };
 
 typedef void (*_slave_handler_ptr)(uint16_t* buffer, uint16_t length, AsyncMST info);
@@ -107,8 +55,9 @@ typedef void (*_SPI_ptr)();
 
 extern SPIClass SPI;
 
-class SPI_MSTransfer_T4_Base {
-  public:
+class SPI_MSTransfer_T4_Base
+{
+public:
     virtual void SPI_MSTransfer_SLAVE_ISR();
 };
 
@@ -120,21 +69,19 @@ static SPI_MSTransfer_T4_Base* _SPI2 = nullptr;
 Circular_Buffer<uint16_t, (uint32_t)pow(2, ceil(log(SPI_MST_QUEUE_SLOTS) / log(2))), SPI_MST_DATA_BUFFER_MAX> mstqueue;
 Circular_Buffer<uint16_t, (uint32_t)pow(2, ceil(log(SPI_MST_QUEUE_SLOTS) / log(2))), SPI_MST_DATA_BUFFER_MAX> smtqueue;
 
-SPI_MSTransfer_T4_CLASS class SPI_MSTransfer_T4 : public SPI_MSTransfer_T4_Base {
-  public:
+SPI_MSTransfer_T4_CLASS
+class SPI_MSTransfer_T4 : public SPI_MSTransfer_T4_Base
+{
+public:
     SPI_MSTransfer_T4();
     void begin();
-    uint16_t transfer16(uint16_t *buffer, uint16_t length, uint16_t packetID);
+    uint16_t transfer16(uint16_t* buffer, uint16_t length, uint16_t packetID);
     void onTransfer(_slave_handler_ptr handler) { _slave_handler = handler; }
     uint32_t events();
     void test();
 
-  private:
-#if defined(KINETISL)
-    volatile uint8_t *spiAddr;
-#else
-    volatile uint32_t *spiAddr;
-#endif
+private:
+    volatile uint32_t* spiAddr;
     void SPI_MSTransfer_SLAVE_ISR();
     uint32_t nvic_irq = 0;
     _slave_handler_ptr _slave_handler;
--- spi_slave_2/src/main.cpp
+++ spi_slave_2/src/main.cpp
@@ -1,27 +1,34 @@
 #include "SPI_MSTransfer_T4.h"
 SPI_MSTransfer_T4<&SPI, 0x1234> mySPI;
 
-void myCB(uint16_t *buffer, uint16_t length, AsyncMST info) {
-    for ( int i = 0; i < length; i++ ) {
-        Serial.print(buffer[i], HEX); Serial.print(" ");
+void myCB(uint16_t* buffer, uint16_t length, AsyncMST info)
+{
+    for (int i = 0; i < length; i++)
+    {
+        Serial.print(buffer[i], HEX);
+        Serial.print(" ");
     }
-    Serial.print(" --> Length: "); Serial.print(length);
-    Serial.print(" --> PacketID: "); Serial.println(info.packetID, HEX);
+    Serial.print(" --> Length: ");
+    Serial.print(length);
+    Serial.print(" --> PacketID: ");
+    Serial.println(info.packetID, HEX);
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     mySPI.begin();
     mySPI.onTransfer(myCB);
 }
 
-void loop() {
+void loop()
+{
     mySPI.events();
     static uint32_t t = millis();
-    if ( millis() - t > 1000 ) {
-        Serial.print("millis: "); Serial.println(millis());
+    if (millis() - t > 1000)
+    {
+        Serial.print("millis: ");
+        Serial.println(millis());
         t = millis();
     }
 }
-
-
